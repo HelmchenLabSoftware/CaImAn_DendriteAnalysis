6b38576fc651347b71521d03b520b3842b56346e
Henry Luetcke <hluetcke@ethz.ch>
2019-04-09 20:21:51 +0200

    use component images in nb_view_patches

--- plotting.py
+++ plotting.py
@@ -1,6 +1,6 @@
 """
 
-Collection of modified plotting utilities from the CaImAn analysis suite. 
+Some modified plotting utilities from the CaImAn analysis suite. 
 
 """
 
@@ -33,130 +33,38 @@ from bokeh.models import CustomJS, ColumnDataSource, Range1d
 from caiman.base.rois import com
 
 
-def get_contours(A, dims, thr=0.9, thr_method='nrg', swap_dim=False):
-    """Gets contour of spatial components and returns their coordinates
-
-     Args:
-         A:   np.ndarray or sparse matrix
-                   Matrix of Spatial components (d x K)
-
-             dims: tuple of ints
-                   Spatial dimensions of movie (x, y[, z])
-
-             thr: scalar between 0 and 1
-                   Energy threshold for computing contours (default 0.9)
-
-             thr_method: [optional] string
-                  Method of thresholding:
-                      'max' sets to zero pixels that have value less than a fraction of the max value
-                      'nrg' keeps the pixels that contribute up to a specified fraction of the energy
-
-     Returns:
-         Coor: list of coordinates with center of mass and
-                contour plot coordinates (per layer) for each component
-    """
-
-    if 'csc_matrix' not in str(type(A)):
-        A = csc_matrix(A)
-    d, nr = np.shape(A)
-    # if we are on a 3D video
-    if len(dims) == 3:
-        d1, d2, d3 = dims
-        x, y = np.mgrid[0:d2:1, 0:d3:1]
-    else:
-        d1, d2 = dims
-        x, y = np.mgrid[0:d1:1, 0:d2:1]
-
-    coordinates = []
-
-    # get the center of mass of neurons( patches )
-    cm = com(A, *dims)
-
-    # for each patches
-    for i in range(nr):
-        pars = dict()
-        # we compute the cumulative sum of the energy of the Ath component that has been ordered from least to highest
-        patch_data = A.data[A.indptr[i]:A.indptr[i + 1]]
-        indx = np.argsort(patch_data)[::-1]
-        if thr_method == 'nrg':
-            cumEn = np.cumsum(patch_data[indx]**2)
-            # we work with normalized values
-            cumEn /= cumEn[-1]
-            Bvec = np.ones(d)
-            # we put it in a similar matrix
-            Bvec[A.indices[A.indptr[i]:A.indptr[i + 1]][indx]] = cumEn
-        else:
-            if thr_method != 'max':
-                warn("Unknown threshold method. Choosing max")
-            Bvec = np.zeros(d)
-            Bvec[A.indices[A.indptr[i]:A.indptr[i + 1]]] = patch_data / patch_data.max()
-
-        if swap_dim:
-            Bmat = np.reshape(Bvec, dims, order='C')
-        else:
-            Bmat = np.reshape(Bvec, dims, order='F')
-        pars['coordinates'] = []
-        # for each dimensions we draw the contour
-        for B in (Bmat if len(dims) == 3 else [Bmat]):
-            vertices = find_contours(B.T, thr)
-            # this fix is necessary for having disjoint figures and borders plotted correctly
-            v = np.atleast_2d([np.nan, np.nan])
-            for _, vtx in enumerate(vertices):
-                num_close_coords = np.sum(np.isclose(vtx[0, :], vtx[-1, :]))
-                if num_close_coords < 2:
-                    if num_close_coords == 0:
-                        # case angle
-                        newpt = np.round(old_div(vtx[-1, :], [d2, d1])) * [d2, d1]
-                        vtx = np.concatenate((vtx, newpt[np.newaxis, :]), axis=0)
-                    else:
-                        # case one is border
-                        vtx = np.concatenate((vtx, vtx[0, np.newaxis]), axis=0)
-                v = np.concatenate(
-                    (v, vtx, np.atleast_2d([np.nan, np.nan])), axis=0)
-
-            pars['coordinates'] = v if len(
-                dims) == 2 else (pars['coordinates'] + [v])
-        pars['CoM'] = np.squeeze(cm[i, :])
-        pars['neuron_id'] = i + 1
-        coordinates.append(pars)
-    return coordinates
-
-
-
-
-def nb_view_patches(Yr, A, C, b, f, d1, d2, YrA=None, image_neurons=None, thr=0.99, denoised_color=None, cmap='jet'):
+def nb_view_patches(Yr, idx, A, C, b, f, d1, d2, YrA=None, image_neurons=None, denoised_color=None, cmap='Greys256', title=''):
     """
     Interactive plotting utility for ipython notebook
 
     Args:
-        Yr: np.ndarray
-            movie
+        Yr: movie
+            
+        idx: indices of components to plot
 
-        A,C,b,f: np.ndarrays
-            outputs of matrix factorization algorithm
+        A,C,b,f: outputs of matrix factorization algorithm
 
-        d1,d2: floats
-            dimensions of movie (x and y)
+        d1,d2: dimensions of movie (x and y)
 
-        YrA:   np.ndarray
-            ROI filtered residual as it is given from update_temporal_components
-            If not given, then it is computed (K x T)
+        YrA:   ROI filtered residual as it is given from update_temporal_components
 
-        image_neurons: np.ndarray
-            image to be overlaid to neurons (for instance the average)
+        image_neurons: image to be overlaid to neurons (for instance the average)
 
-        thr: double
-            threshold regulating the extent of the displayed patches
+        denoised_color: color name (e.g. 'red') or hex color code (e.g. '#F0027F')
 
-        denoised_color: string or None
-            color name (e.g. 'red') or hex color code (e.g. '#F0027F')
-
-        cmap: string
-            name of colormap (e.g. 'viridis') used to plot image_neurons
+        cmap: name of colormap (e.g. 'viridis') used to plot image_neurons
     """
+    
+    A = A[:,idx]
+    C = C[idx]
+    YrA = YrA[idx]
+    
+    # compute component images
+    component_images = []
+    A_dense = A.todense()
+    for ix in range(len(idx)):
+        component_images.append(np.flipud(np.array(np.reshape(A_dense[:,ix], (d1,d2), order='F'))))
 
-    colormap = cm.get_cmap(cmap)
-    grayp = [mpl.colors.rgb2hex(m) for m in colormap(np.arange(colormap.N))]
     nr, T = C.shape
     nA2 = np.ravel(np.power(A, 2).sum(0)) if type(
         A) == np.ndarray else np.ravel(A.power(2).sum(0))
@@ -174,87 +82,51 @@ def nb_view_patches(Yr, A, C, b, f, d1, d2, YrA=None, image_neurons=None, thr=0.
     if image_neurons is None:
         image_neurons = A.mean(1).reshape((d1, d2), order='F')
 
-    coors = get_contours(A, (d1, d2), thr)
-    # adjust coordinates
-    for ix, cor in enumerate(coors):
-        patch = cor['coordinates']
-        # find and remove NaN coordinates
-        nan_ix = np.unique(np.argwhere(np.isnan(patch))[:,0])
-        patch = np.delete(patch, nan_ix, 0)
-        # flip rows upside down
-        patch[:,1] = d1 - patch[:,1]
-        coors[ix]['coordinates'] = patch
+    source_line = ColumnDataSource(data=dict(x=x, y=Y_r[0] / 100, y2=C[0] / 100))
+    source_line_ = ColumnDataSource(data=dict(z=Y_r / 100, z2=C / 100))
+    source_img = ColumnDataSource(data=dict(img=[component_images[0]]))
+    source_img_ = ColumnDataSource(data=dict(img=component_images))
     
-    cc1 = [cor['coordinates'][:, 0] for cor in coors]
-    cc2 = [cor['coordinates'][:, 1] for cor in coors]
-    c1 = cc1[0]
-    c2 = cc2[0]
-
-    # split sources up, such that Bokeh does not warn
-    # "ColumnDataSource's columns must be of the same length"
-    source = ColumnDataSource(data=dict(x=x, y=Y_r[0] / 100, y2=C[0] / 100))
-    source_ = ColumnDataSource(data=dict(z=Y_r / 100, z2=C / 100))
-    source2 = ColumnDataSource(data=dict(c1=c1, c2=c2))
-    source2_ = ColumnDataSource(data=dict(cc1=cc1, cc2=cc2))
-
-    callback = CustomJS(args=dict(source=source, source_=source_, source2=source2, source2_=source2_), code="""
-            var data = source.data
-            var data_ = source_.data
-            var f = cb_obj.value - 1
-            x = data['x']
-            y = data['y']
-            y2 = data['y2']
-
-            for (i = 0; i < x.length; i++) {
-                y[i] = data_['z'][i+f*x.length]
-                y2[i] = data_['z2'][i+f*x.length]
+    callback = CustomJS(args=dict(source_line=source_line, source_line_=source_line_, source_img=source_img, source_img_=source_img_), code="""
+        var line_data = source_line.data;
+        var line_data_ = source_line_.data;
+        var img_data = source_img.data;
+        var img_data_ = source_img_.data;
+        var f = cb_obj.value - 1;
+        
+        var x = line_data['x'];
+        var y = line_data['y'];
+        var y2 = line_data['y2'];
+        
+        for (i = 0; i < x.length; i++) {
+                y[i] = line_data_['z'][i+f*x.length];
+                y2[i] = line_data_['z2'][i+f*x.length];
             }
-
-            var data2_ = source2_.data;
-            var data2 = source2.data;
-            c1 = data2['c1'];
-            c2 = data2['c2'];
-            cc1 = data2_['cc1'];
-            cc2 = data2_['cc2'];
-
-            for (i = 0; i < c1.length; i++) {
-                   c1[i] = cc1[f][i]
-                   c2[i] = cc2[f][i]
-            }
-            source2.change.emit();
-            source.change.emit();
-        """)
-
+            
+        img = img_data_['img']
+        
+        source_img.data['img'] = [img[f]]
+        
+        source_line.change.emit();
+        source_img.change.emit();
+    """)
+    
     plot = bpl.figure(plot_width=600, plot_height=300)
-    plot.line('x', 'y', source=source, line_width=1, line_alpha=0.6)
+    plot.line('x', 'y', source=source_line, line_width=1, line_alpha=0.6)
     if denoised_color is not None:
-        plot.line('x', 'y2', source=source, line_width=1,
+        plot.line('x', 'y2', source=source_line, line_width=1,
                   line_alpha=0.6, color=denoised_color)
-
-    slider = bokeh.models.Slider(start=1, end=Y_r.shape[0], value=1, step=1,
-                                 title="Neuron Number", callback=callback)
-    xr = Range1d(start=0, end=image_neurons.shape[1])
-    yr = Range1d(start=image_neurons.shape[0], end=0)
-    plot1 = bpl.figure(plot_width=image_neurons.shape[1]*2, plot_height=image_neurons.shape[0]*2, toolbar_location="below")
-
-#     plot1.image(image=[image_neurons[::-1, :]], x=0,
-#                 y=image_neurons.shape[0], dw=d2, dh=d1, palette=grayp)
     
-    plot1.image(image=[np.flipud(image_neurons)], x=0, y=0, dw=image_neurons.shape[1], dh=image_neurons.shape[0], palette=grayp)
+    plot1 = bpl.figure(plot_width=image_neurons.shape[1]*2, plot_height=image_neurons.shape[0]*2, 
+                       x_range = [0, image_neurons.shape[1]], y_range = [0, image_neurons.shape[0]], 
+                       toolbar_location="below", title=title)
     
-    plot1.patch('c1', 'c2', alpha=0.6, color='purple',
-                line_width=2, source=source2)
-
-    plot1.x_range = Range1d(0, image_neurons.shape[1])
-    plot1.y_range = Range1d(0, image_neurons.shape[0])
+    plot1.image('img', source=source_img, x=0, y=0, dw=image_neurons.shape[1], dh=image_neurons.shape[0], palette=cmap)
+    
+    slider = bokeh.models.Slider(start=1, end=Y_r.shape[0], value=1, step=1, title="Neuron Number", callback=callback)
     
     grid = gridplot([[plot1, None], [plot, slider]], sizing_mode='fixed', toolbar_location='left')
     
     bpl.show(grid)
     
-    return coors
-    
-#     if Y_r.shape[0] > 1:
-#         bpl.show(bokeh.layouts.layout([[slider], [bokeh.layouts.row(plot1, plot)]]))
-#     else:
-#         bpl.show(bokeh.layouts.row(plot1, plot))
+    return component_images
